aa9a8d901bf50302e674f139e32f6b88dae838e0
JamesDowney <james.m.downey30@gmail.com>
2022-12-03 11:07:24 -0700

    final solution

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,48 @@ import { bufferToFile, fileToBuffer, print } from "kolmafia";
 
 export default function main(): void {
   const input = fileToBuffer("input.txt").trim().split(/\n/g);
-  const solution = input;
-  solution.forEach((element) => {
-    print(element);
+  let score = 0;
+  let decryptedScore = 0;
+  const values = new Map<string, number>([
+    ["A", 3], // rock
+    ["B", 2], // paper
+    ["C", 1], // scissors
+    ["X", 1], // rock
+    ["Y", 2], // paper
+    ["Z", 3], // scissors
+  ]);
+  const goals = new Map([
+    ["A", ["Z", "X", "Y"]],
+    ["B", ["X", "Y", "Z"]],
+    ["C", ["Y", "Z", "X"]],
+  ]);
+
+  input.forEach((element) => {
+    const opponent = values.get(element[0])!;
+    const you = values.get(element[2])!;
+    const outcome = (you + opponent) % 3;
+    if (outcome === 0) {
+      score += you;
+    } else if (outcome === 1) {
+      score += you + 3;
+    } else if (outcome === 2) {
+      score += you + 6;
+    }
   });
-  bufferToFile(solution.join("\n"), "output.txt");
+
+  input.forEach((element) => {
+    const opponent = element[0]!;
+    const outcome = values.get(element[2])! - 1;
+    const you = values.get(goals.get(opponent)![outcome])!;
+    if (outcome === 0) {
+      decryptedScore += you;
+    } else if (outcome === 1) {
+      decryptedScore += you + 3;
+    } else if (outcome === 2) {
+      decryptedScore += you + 6;
+    }
+  });
+
+  print("your score is: " + score);
+  print("your decrypted score is: " + decryptedScore);
 }
